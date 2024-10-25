de97183c66991136b84bc12bc578c9e85b19f3e0
tangent248 <abhishekkumar355ak@gmail.com>
2024-10-25 22:19:35 +0530

    uploaded dockerfile

--- main.cpp
+++ main.cpp
@@ -1,7 +1,71 @@
-#include <iostream>
-#include<SDL2/SDL.h>
-int main(){
-std::cout << "hello world";
+// On linux compile with:
+// g++ -std=c++17 main.cpp -o prog -lSDL2
 
-return 0;
+// C++ Standard Libraries
+#include <iostream>
+
+// Third-party library
+#include <SDL2/SDL.h>
+
+int main(int argc, char* argv[]){
+
+    // Create a window data type
+    // This pointer will point to the 
+    // window that is allocated from SDL_CreateWindow
+    SDL_Window* window=nullptr;
+
+    // Initialize the video subsystem.
+    // If it returns less than 1, then an
+    // error code will be received.
+    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+        std::cout << "SDL could not be initialized: " <<
+                  SDL_GetError();
+    }else{
+        std::cout << "SDL video system is ready to go\n";
+    }
+
+    // Request a window to be created for our platform
+    // The parameters are for the title, x and y position,
+    // and the width and height of the window.
+    window = SDL_CreateWindow("C++ SDL2 Window",
+            0,
+            2500,
+            640,
+            480,
+            SDL_WINDOW_SHOWN);
+
+    // Infinite loop for our application
+    bool gameIsRunning = true;
+    while(gameIsRunning){
+        SDL_Event event;
+        // Start our event loop
+        while(SDL_PollEvent(&event)){
+            // Handle each specific event
+            if(event.type == SDL_QUIT){
+                gameIsRunning= false;
+            }
+            if(event.type == SDL_MOUSEMOTION){
+                std::cout << "mouse has been moved\n";
+            }
+            if(event.type == SDL_KEYDOWN){
+                std::cout << "a key has been pressed\n";
+                if(event.key.keysym.sym == SDLK_0){
+                    std::cout << "0 was pressed\n";
+                }else{
+                    std::cout << "0 was not pressed\n";
+                }
+            }
+            
+            const Uint8* state = SDL_GetKeyboardState(NULL);
+            if(state[SDL_SCANCODE_RIGHT]){
+                std::cout << "right arrow key is pressed\n";
+            }
+
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    
+    SDL_Quit();
+    return 0;
 }
