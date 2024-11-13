9b38f5f9c959af11412ceea8797f1000a996bb1f
tangent248 <abhishekkumar355ak@gmail.com>
2024-11-13 17:16:26 +0530

    new

---
+++ Game.h
@@ -0,0 +1,25 @@
+#include "SDL2/SDL.h"
+#ifndef __Game__
+#define __Game__
+
+class Game {
+
+public:
+  Game() {}
+  ~Game() {}
+
+  bool init(const char *title, int xpos, int ypos, int width, int height,
+            bool fullscreen);
+  void render();
+  void update();
+  void handleEvents();
+  void clean();
+  // a function to access the private running variable
+  bool running() { return m_bRunning; }
+
+private:
+  SDL_Window *m_pWindow;
+  SDL_Renderer *m_pRenderer;
+  bool m_bRunning;
+};
+#endif /* defined(__Game__) */
